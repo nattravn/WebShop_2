596f067990a7bdff6ebcac433ca72c04a1ce1aa0
Erik Olsson <erik.olsson627@gmail.com>
2023-07-29 16:51:43 +0200

    import configurations

--- WebShopAdmin/src/app/admin-panel/stores/clothing.store.ts
+++ WebShopAdmin/src/app/admin-panel/stores/clothing.store.ts
@@ -1,99 +1,103 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Clothing } from "@admin/models/clothing.model";
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 
-import { Observable, ReplaySubject } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { ToastrService } from "ngx-toastr";
+import { Observable, ReplaySubject } from "rxjs";
+import { tap } from "rxjs/operators";
 
-import { Clothing } from '../models/clothing.model';
-import { environment } from 'src/environments/environment';
-import { ToastrService } from 'ngx-toastr';
+import { environment } from "@environments/environment";
 
 @Injectable({
-	providedIn: 'root'
+	providedIn: "root",
 })
 export class ClothingStore {
 	readonly baseUrl = environment.baseUrl;
-	public imageRootPath = environment.baseUrl + '/Images/original/';
-	public defaultimageRootPath = environment.baseUrl + '/Images/original/default-image.png';
+
+	public imageRootPath = `${environment.baseUrl}/Images/original/`;
+
+	public defaultimageRootPath = `${environment.baseUrl}/Images/original/default-image.png`;
+
 	list: Clothing[];
+
 	private clothingsReplay: ReplaySubject<Clothing[]> = new ReplaySubject<Clothing[]>(1);
-	public clothingsReplay$: Observable<Clothing[]> = this.clothingsReplay.asObservable();
 
 	private clothingItemReplay: ReplaySubject<Clothing> = new ReplaySubject<Clothing>(1);
 
-	public clothingItemReplay$: Observable<Clothing> = this.clothingItemReplay.asObservable();
-
-	constructor(private http: HttpClient,  private toastr: ToastrService) { }
-
+	constructor(
+		private http: HttpClient,
+		private toastr: ToastrService,
+	) {}
 
 	getClothings(): Observable<Clothing[]> {
-		return this.http.get<Clothing[]>(this.baseUrl + '/Clothings').pipe(tap(items => {
-			this.clothingsReplay.next(items);
-		}));
+		return this.http.get<Clothing[]>(`${this.baseUrl}/Clothings`).pipe(
+			tap((items) => {
+				this.clothingsReplay.next(items);
+			}),
+		);
 	}
 
 	getClothing(id: number): Observable<Clothing> {
-		return this.http.get<Clothing>(this.baseUrl + '/Clothings/' + id).pipe(tap(item => {
-			this.clothingItemReplay.next(item);
-		}));
+		return this.http.get<Clothing>(`${this.baseUrl}/Clothings/${id}`).pipe(
+			tap((item) => {
+				this.clothingItemReplay.next(item);
+			}),
+		);
 	}
 
 	postClothing(modelFormData: Clothing, fileToUpload: File) {
 		const formData: FormData = new FormData();
 
-		formData.append('title', modelFormData.title);
-		formData.append('price', modelFormData.price.toString());
-		formData.append('size', modelFormData.size);
-		formData.append('description', modelFormData.description);
-		formData.append('image', fileToUpload, fileToUpload.name);
-		formData.append('imagePath', modelFormData.imagePath);
-		formData.append('category', modelFormData.categoryId.toString());
-		formData.append('userId', modelFormData.editorUserId.toString());
-		formData.append('subCategory', modelFormData.subCategoryId.toString());
-		formData.append('userName', modelFormData.userName);
+		formData.append("title", modelFormData.title);
+		formData.append("price", modelFormData.price.toString());
+		formData.append("size", modelFormData.size);
+		formData.append("description", modelFormData.description);
+		formData.append("image", fileToUpload, fileToUpload.name);
+		formData.append("imagePath", modelFormData.imagePath);
+		formData.append("category", modelFormData.categoryId.toString());
+		formData.append("userId", modelFormData.editorUserId.toString());
+		formData.append("subCategory", modelFormData.subCategoryId.toString());
+		formData.append("userName", modelFormData.userName);
 
-		return this.http.post(this.baseUrl + '/Clothings', formData);
+		return this.http.post(`${this.baseUrl}/Clothings`, formData);
 	}
 
 	putClothing(modelFormData: Clothing, fileToUpload: File) {
 		const formData: FormData = new FormData();
-		console.log("modelFormData: ", modelFormData)
-		formData.append('title', modelFormData.title);
-		formData.append('id', modelFormData.id.toString());
-		formData.append('price', modelFormData.price.toString());
-		formData.append('size', modelFormData.size);
-		formData.append('description', modelFormData.description);
+		console.log("modelFormData: ", modelFormData);
+		formData.append("title", modelFormData.title);
+		formData.append("id", modelFormData.id.toString());
+		formData.append("price", modelFormData.price.toString());
+		formData.append("size", modelFormData.size);
+		formData.append("description", modelFormData.description);
 		if (fileToUpload != null) {
-			formData.append('Image', fileToUpload, fileToUpload.name);
+			formData.append("Image", fileToUpload, fileToUpload.name);
 		}
-		formData.append('imagePath', modelFormData.imagePath);
-		formData.append('category', modelFormData.categoryId.toString());
-		formData.append('userId', modelFormData.editorUserId.toString());
-		formData.append('subCategory', modelFormData.subCategoryId.toString());
+		formData.append("imagePath", modelFormData.imagePath);
+		formData.append("category", modelFormData.categoryId.toString());
+		formData.append("userId", modelFormData.editorUserId.toString());
+		formData.append("subCategory", modelFormData.subCategoryId.toString());
 
-		return this.http.put(
-			this.baseUrl + '/Clothings/' + modelFormData.id,
-			formData
-		).pipe(tap(x => {
-			this.toastr.info('updated successfully', `Clothing:  ${modelFormData.title}`);
-		}))
+		return this.http.put(`${this.baseUrl}/Clothings/${modelFormData.id}`, formData).pipe(
+			tap(() => {
+				this.toastr.info("updated successfully", `Clothing:  ${modelFormData.title}`);
+			}),
+		);
 	}
 
 	deleteClothing(id: number) {
-		return this.http.delete(this.baseUrl + '/Clothings/' + id);
+		return this.http.delete(`${this.baseUrl}/Clothings/${id}`);
 	}
 
 	getClothingByUserName(userName: string): Observable<Clothing[]> {
-		return this.http.get<Clothing[]>(
-			this.baseUrl + '/Clothings/username/' + userName
-		);
+		return this.http.get<Clothing[]>(`${this.baseUrl}/Clothings/username/${userName}`);
 	}
 
 	refreshList() {
 		this.http
-			.get(this.baseUrl + '/Clothings')
+			.get(`${this.baseUrl}/Clothings`)
 			.toPromise()
-			.then(res => {
+			.then((res) => {
 				this.list = res as Clothing[];
 			});
 	}
--- WebShopAdmin/src/app/app.module.ts
+++ WebShopAdmin/src/app/app.module.ts
@@ -1,21 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AdminPanelModule } from './admin-panel/admin-panel.module';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthComponent } from './log-in/auth/auth.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { ToastrModule } from 'ngx-toastr';
-import { AuthInterceptor } from './guard/auth.interceptor';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AdminPanelModule } from "./admin-panel/admin-panel.module";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { AuthComponent } from "./log-in/auth/auth.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { ToastrModule } from "ngx-toastr";
+import { AuthInterceptor } from "./guard/auth.interceptor";
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		AuthComponent,
-	],
+	declarations: [AppComponent, AuthComponent],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
@@ -25,16 +22,16 @@ import { AuthInterceptor } from './guard/auth.interceptor';
 		HttpClientModule,
 		ToastrModule.forRoot({
 			timeOut: 2000,
-			positionClass: 'toast-top-right'
+			positionClass: "toast-top-right",
 		}),
 	],
 	providers: [
 		{
-            provide: HTTP_INTERCEPTORS,
-            useClass: AuthInterceptor,
-            multi: true
-        },
+			provide: HTTP_INTERCEPTORS,
+			useClass: AuthInterceptor,
+			multi: true,
+		},
 	],
-	bootstrap: [AppComponent]
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
